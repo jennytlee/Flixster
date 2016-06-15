0c02f61a114c51141b1f738bc69ade9c733cf3a0
Jenny Lee <jennytoralee@gmail.com>
2016-06-15 11:56:26 -0700

    First commit

---
+++ app/src/main/java/com/example/jennytlee/flickster/MovieActivity.java
@@ -0,0 +1,54 @@
+package com.example.jennytlee.flickster;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+
+        // Retrieve movies
+        ArrayList<Movie> movies = Movie.getFakeMovies();
+
+        // Generate ListView to populate
+        ListView lvMovies = (ListView) findViewById(R.id.lvMovies);
+
+        // Create ArrayAdapter
+        //ArrayAdapter<Movie> adapter = new ArrayAdapter<Movie>(this, android.R.layout.simple_list_item_1, movies);
+        MoviesAdapter adapter = new MoviesAdapter(this, movies);
+
+        // Associate ArrayAdapter with ListView
+        if (lvMovies != null) {
+            lvMovies.setAdapter(adapter);
+        }
+
+        String url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(url, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
+    }
+}
